98467ce26ae3e72c24520437cfa0a81fd81ba32e
Nguyễn Hoàng Hiệp <nguyenhoanghiep478@gmail.com>
2024-09-24 22:24:00 +0700

    deploy-database

--- Back-end/TrainTicketBooking/src/main/java/com/backend/trainticketbooking/services/config-server/src/main/java/com/bookms/config_server/ConfigServerApplication.java
+++ Back-end/TrainTicketBooking/src/main/java/com/backend/trainticketbooking/services/config-server/src/main/java/com/bookms/config_server/ConfigServerApplication.java
@@ -1,16 +1,19 @@
 package com.bookms.config_server;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigServerApplication {
 
-
 	public static void main(String[] args) {
-
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("PROD_DB_URL", dotenv.get("PROD_DB_URL"));
+		System.setProperty("PROD_DB_USERNAME", dotenv.get("PROD_DB_USERNAME"));
+		System.setProperty("PROD_DB_PASSWORD", dotenv.get("PROD_DB_PASSWORD"));
+		System.setProperty("PROD_ADDITIONAL_PROP", dotenv.get("PROD_ADDITIONAL_PROP"));
 		SpringApplication.run(ConfigServerApplication.class, args);
 	}
 
